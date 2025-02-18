da66521f221625ebe238c8fc35770e646f7de2c7
Mikita <bansanrock@gmail.com>
2025-02-18 05:07:35 +0300

    ref: incorrect task

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { RandomValues } from "./random-values/random-values.class";
+
+function bootstrap() {
+  try {
+    const from = 100;
+    const to = 399;
+
+    const randomValueFirst = new RandomValues({
+      from,
+      to,
+    }).getRandomValue();
+
+    console.log(`First random value: ${randomValueFirst}`);
+  } catch (error) {
+    console.log(`Error while execution 'bootstrap' ${error}`);
+  }
+}
+
+bootstrap();
