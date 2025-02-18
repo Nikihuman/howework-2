a1df2c7947fb01d1c1384ce08637133a3a1fceac
Mikita <bansanrock@gmail.com>
2025-02-18 12:15:21 +0300

    edits

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { RandomValues } from "./random-values/random-values.class";
 function bootstrap() {
   try {
     const from = 100;
-    const to = 399;
+    const to = 500;
 
     const randomValueFirst = new RandomValues({
       from,
