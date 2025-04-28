af5319d53c6f1e3ebcf9aa5ae109c47a14e642cd
Mikita <112314400+Nikihuman@users.noreply.github.com>
2025-04-28 20:38:08 +0300

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { RandomValues } from "./random-values/random-values.class";
+
+function bootstrap() {
+  try {
+    const from = 100;
+    const to = 1000;
+
+    const randomValueFirst = new RandomValues({
+      from,
+      to,
+    }).getRandomValue();
+
+    console.log(`First random value: ${randomValueFirst}`);
+  } catch (error) {
+    console.log(`Error while execution 'bootstrap' ${error}`);
+  }
+}
+
+
+// Короень слова корень ?
+
+bootstrap();
